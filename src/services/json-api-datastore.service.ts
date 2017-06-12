d293994fffabb829da51ce3891213c041b2e61f4
nickdelja <nickdelja@gmail.com>
2017-06-12 15:07:24 -0700

    Allow setting of base url

--- src/services/json-api-datastore.service.ts
+++ src/services/json-api-datastore.service.ts
@@ -309,6 +309,10 @@ export class JsonApiDatastore {
             }
         }
         return model;
-    };
+    }
+
+    public setBaseUrl(baseUrl: string): void {
+        Reflect.getMetadata('JsonApiDatastoreConfig', this.constructor).baseUrl = baseUrl;
+    }
 
 }
